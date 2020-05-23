6186104a2d5fcda0913e77225c5ffe503dcb07cb
Brenda-M <brendanjuguna1@gmail.com>
2020-05-23 15:20:46 +0300

    add a link and image column to the project model

--- mainpage/models.py
+++ mainpage/models.py
@@ -4,6 +4,10 @@ class Project(models.Model):
   title = models.CharField(max_length=50)
   description = models.CharField(max_length=250)
   technology = models.CharField(max_length=50)
+  project_image = models.ImageField(upload_to = 'projects/', blank=True)
+  project_link = models.CharField(max_length=500)
+
 
   def __str__(self):
     return self.title
+
