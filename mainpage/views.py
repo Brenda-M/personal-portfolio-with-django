b412508dabec8347b2d7a1871c08bbbe57d59fe4
Brenda-M <brendanjuguna1@gmail.com>
2020-05-24 00:16:42 +0300

    add the form to the function based view

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .models import Project, Tag
+from .forms import ContactForm
 
 
 def home(request):
 
+  # if request.method == 'POST':
+  #   form = ContactForm(request.POST)
+  #   if form.is_valid():
+  #     return HttpResponse('Thanks you for your message')
+  # else: 
+  form = ContactForm()
+
   projects = Project.objects.all()
+  context ={
+    'form':form,
+    'projects': projects
+  }
 
 
-  return render(request, 'mainpage/main.html', {'projects': projects})
+  return render(request, 'mainpage/main.html', context)
