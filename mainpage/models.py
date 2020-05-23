23ec01b7ebb688ca1a737749477e3dc226be1d7f
Brenda-M <brendanjuguna1@gmail.com>
2020-05-23 17:15:29 +0300

    created a tag class

--- mainpage/models.py
+++ mainpage/models.py
@@ -3,11 +3,19 @@ from django.db import models
 class Project(models.Model):
   title = models.CharField(max_length=50)
   description = models.CharField(max_length=250)
-  technology = models.CharField(max_length=50)
-  project_image = models.ImageField(upload_to = 'projects/', blank=True)
-  project_link = models.CharField(max_length=500)
+  project_image = models.ImageField(upload_to = 'mainpage/images/', blank=True)
+  project_link = models.URLField(max_length=250, blank=True)
+  pub_date = models.DateTimeField(auto_now_add=True)
+  tag = models.ManyToManyField(Tag)
 
 
   def __str__(self):
     return self.title
+
+class Tag (models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+
 
