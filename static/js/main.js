affdf451c430342953b812e6977aeb4c11d6ec0d
Brenda-M <brendanjuguna1@gmail.com>
2020-05-23 22:25:03 +0300

    add toggle effect to services section

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,41 @@ $(document).ready(function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+  $("#designDescription").hide();
+  $("#developmentDescription").hide();
+  $("#productDescripton").hide();
+
+  $("#designImage").click(function () {
+    $("#designImage").hide();
+    $("#designDescription").show();
+  });
+
+  $("#designDescription").click(function () {
+    $("#designDescription").hide();
+    $("#designImage").show();
+  });
+
+  $("#developmentImage").click(function(){
+    $("#developmentImage").hide();
+    $("#developmentDescription").show();
+  });
+
+  $("#developmentDescription").click(function(){
+    $("#developmentDescription").hide();
+    $("#developmentImage").show();
+  });
+  
+  $("#productImage").click(function (){
+    $("#productImage").hide()
+    $("#productDescripton").show()
+  });
+
+  $("#productDescripton").click(function (){
+    $("#productDescripton").hide()
+    $("#productImage").show()
+  });
+
+
+
 });
