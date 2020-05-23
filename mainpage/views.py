bbcc1310518ac379ca8f2a1a598e94dd2d5ec86f
Brenda-M <brendanjuguna1@gmail.com>
2020-05-24 02:05:27 +0300

    add flash message

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,18 +1,28 @@
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Project, Tag
 from .forms import ContactForm
 
 
 def home(request):
+  
+  if request.method == 'POST':
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      name= form.cleaned_data.get("name")
+      email= form.cleaned_data.get("email")
+      message=form.cleaned_data.get("message")
+      
+      comment = f"Incoming message from {name}. The message is as stated below. \n\n" + message;
+      send_mail('New Enquiry', comment, email, ['noreplaymail84@gmail.com'])
 
-  # if request.method == 'POST':
-  #   form = ContactForm(request.POST)
-  #   if form.is_valid():
-  #     return HttpResponse('Thanks you for your message')
-  # else: 
-  form = ContactForm()
+      messages.add_message(request, messages.SUCCESS, "Your message has been sent.")
+
+      return redirect('home')
+  else: 
+    form = ContactForm()
 
   projects = Project.objects.all()
   context ={
@@ -20,5 +30,9 @@ def home(request):
     'projects': projects
   }
 
+  return render(request, 'mainpage/main.html', context )
 
-  return render(request, 'mainpage/main.html', context)
+# def success(request):
+#   message = 'Thank you for your message'
+
+#   return render(request, 'mainpage/test.html', {'message':message})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,6 @@
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 ROOT_URLCONF = 'portfolio.urls'
 
@@ -119,3 +122,12 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Email Configurations
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+
