5a2ed5ecbdb570ef0908e90c0d4a398795c1182a
Brenda-M <brendanjuguna1@gmail.com>
2020-05-23 19:44:45 +0300

    reset global margin and padding

--- mainpage/models.py
+++ mainpage/models.py
@@ -22,3 +22,5 @@ class Project(models.Model):
 
 
 
+
+
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Tag
 
 
 def home(request):
 
   projects = Project.objects.all()
+
 
   return render(request, 'mainpage/main.html', {'projects': projects})
