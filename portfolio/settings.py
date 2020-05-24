6d1344b748ed2a843e54bd4f57de44200fa35769
Brenda-M <brendanjuguna1@gmail.com>
2020-05-24 23:34:04 +0300

    declare allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,10 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    config('ALLOWED_HOSTS')
+    '.localhost',
+    '.herokuapp.com', 
+    '.127.0.0.1'
+    # config('ALLOWED_HOSTS')
 ]
 
 
