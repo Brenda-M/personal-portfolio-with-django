b9c34245ec6517559b10a36c85a0677c1d8860eb
Brenda-M <brendanjuguna1@gmail.com>
2020-05-25 07:54:20 +0300

    change database configurations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,7 +1,7 @@
 
 import os
-# import django_heroku
-# import dj_database_url
+import django_heroku
+import dj_database_url
 from decouple import config
 
 
@@ -22,10 +22,7 @@ SECRET_KEY = '0$6%v$_7rcdu=18!bxguystup7@08ou96lt6^1n%ar9wgd=rr!'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    # '.localhost',
-    # '.herokuapp.com', 
-    # '.127.0.0.1'
-    # config('ALLOWED_HOSTS')
+    config('ALLOWED_HOSTS'),
 ]
 
 
@@ -80,29 +77,29 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         # 'NAME': os.environ.get('DB_NAME', 'postgres'),
-#         # 'USER': os.environ.get('DB_USER', 'postgres'),
-#         # 'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
-#         # 'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         # 'PORT': os.environ.get('DB_PORT', '5432'),
-#         # 'TEST': {
-#         #     'NAME': 'test_<app_name>'
-#         # },
-#         'USER': 'postgres',
-#         'PASSWORD':'Bm19952810'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio',
-        'USER': 'postgres',
-        'PASSWORD': 'Bm19952810'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_<app_name>'
+        },
+        # 'USER': 'postgres',
+        # 'PASSWORD':'Bm19952810'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfolio',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Bm19952810'
+#     }
+# }
 
 
 
@@ -155,7 +152,7 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -169,5 +166,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # EMAIL_BACKEND = config('EMAIL_BACKEND')
 
 # Configure Django App for Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
 
