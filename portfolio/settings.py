661a3ec9ca946f87386d36438d3650879db2e7a4
brenda-m <brendanjuguna1@gmail.com>
2023-10-21 01:39:08 +0300

    downgrade psycopg2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,8 +1,216 @@
 
+# import os
+# import django_heroku
+# import dj_database_url
+# from decouple import config
+
+
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# # SECRET_KEY = config('SECRET_KEY')
+# # SECRET_KEY=config('SECRET_KEY') 
+
+# SECRET_KEY='0$6%v$_7rcdu=18!bxguystup7@08ou96lt6^1n%ar9wgd=rr'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = config('DEBUG', default=False, cast=bool)
+
+# MODE=config("MODE", default="dev")
+
+# ALLOWED_HOSTS = [
+#     "brenda-muthoni-portfolio-64e73d572a8b.herokuapp.com", 
+#     "127.0.0.1",
+#     "localhost"
+# ]
+
+
+# # Application definition
+
+# INSTALLED_APPS = [
+#     'mainpage.apps.MainpageConfig',
+#     'bootstrap4',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ]
+
+# MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
+# ROOT_URLCONF = 'portfolio.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#                 'django.template.context_processors.media',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'portfolio.wsgi.application'
+
+
+# # Database
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+# #         'NAME': os.environ.get('DB_NAME', 'postgres'),
+# #         'USER': os.environ.get('DB_USER', 'postgres'),
+# #         'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+# #         'HOST': os.environ.get('DB_HOST', 'localhost'),
+# #         'PORT': os.environ.get('DB_PORT', '5432'),
+# #         'TEST': {
+# #             'NAME': 'test_<app_name>'
+# #         },
+# #         'USER': 'postgres',
+# #         'PASSWORD':'Bm19952810'
+# #     }
+# # }
+
+# # DATABASES = {
+# #         'default': dj_database_url.config(
+# #             default='postgres://itsbrenda_muthoni:cyZ3HEVA1lsUHbc@itsbrenda-muthoni-db.flycast:5432/itsbrenda_muthoni?sslmode=disable'
+# #         )
+# #     }
+
+# if config('MODE') == "dev":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': config('DB_HOST'),
+#             'PORT': '',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.config(
+#             default=config('DATABASE_URL')
+#         )
+#     }
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+
+
+
+
+
+# # Password validation
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+
+# # Internationalization
+# # https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'UTC'
+
+# USE_I18N = True
+
+# USE_L10N = True
+
+# USE_TZ = True
+
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_URL = '/static/'
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+# # Simplified static file serving.
+# # https://warehouse.python.org/project/whitenoise/
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# #Email Configurations
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# # EMAIL_BACKEND = config('EMAIL_BACKEND')
+
+# # Configure Django App for Heroku.
+# django_heroku.settings(locals())
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+
+"""
+Django settings for ip_address_management_system project.
+
+Generated by 'django-admin startproject' using Django 2.2.12.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.2/ref/settings/
+"""
+
 import os
-import django_heroku
 import dj_database_url
+import django_heroku
 from decouple import config
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -10,15 +218,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY=config('SECRET_KEY') 
+SECRET_KEY = 'n-21*dlqyshd6m!yd!f(p4aqpjk7h#xg@l)aor+_)9prvvw5dg'
 
-SECRET_KEY='0$6%v$_7rcdu=18!bxguystup7@08ou96lt6^1n%ar9wgd=rr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 MODE=config("MODE", default="dev")
@@ -33,6 +237,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+
     'mainpage.apps.MainpageConfig',
     'bootstrap4',
     'django.contrib.admin',
@@ -41,9 +246,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,14 +261,12 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-
 ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +274,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
             ],
         },
     },
@@ -79,29 +283,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME', 'postgres'),
-#         'USER': os.environ.get('DB_USER', 'postgres'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
-#         'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#         'TEST': {
-#             'NAME': 'test_<app_name>'
-#         },
-#         'USER': 'postgres',
-#         'PASSWORD':'Bm19952810'
-#     }
-# }
-
-# DATABASES = {
-#         'default': dj_database_url.config(
-#             default='postgres://itsbrenda_muthoni:cyZ3HEVA1lsUHbc@itsbrenda-muthoni-db.flycast:5432/itsbrenda_muthoni?sslmode=disable'
-#         )
-#     }
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 if config('MODE') == "dev":
     DATABASES = {
@@ -125,12 +307,8 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-
-
-
-
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -149,7 +327,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -161,12 +339,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+TAILWIND_APP_NAME = 'theme'
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
@@ -174,22 +357,12 @@ STATICFILES_DIRS = [
 ]
 
 # Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
+# https://warehouse.python.org/project/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#Email Configurations
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_BACKEND = config('EMAIL_BACKEND')
-
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
-
-db_from_env = dj_database_url.config(conn_max_age=500)
